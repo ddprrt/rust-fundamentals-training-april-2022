e7fadfbc9042b276dc7693a84789c46971b8f451
Stefan Baumgartner <sbaumg@gmail.com>
2022-04-05 21:10:58 +0200

    add solution

--- solutions/nth-prime/src/main.rs
+++ solutions/nth-prime/src/main.rs
@@ -1,5 +1,27 @@
+fn is_prime(x: u32) -> bool {
+    for i in 2..x {
+        if x % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
 pub fn nth(n: u32) -> u32 {
-    todo!("Implement this method")
+    let mut count = 0;
+    let mut num = 2;
+    loop {
+        if count == n {
+            break;
+        };
+
+        num = num + 1;
+
+        if is_prime(num) {
+            count = count + 1;
+        }
+    }
+    num
 }
 
 fn main() {
@@ -12,19 +34,16 @@ fn test_first_prime() {
 }
 
 #[test]
-#[ignore]
 fn test_second_prime() {
     assert_eq!(nth(1), 3);
 }
 
 #[test]
-#[ignore]
 fn test_sixth_prime() {
     assert_eq!(nth(5), 13);
 }
 
 #[test]
-#[ignore]
 fn test_big_prime() {
     assert_eq!(nth(10_000), 104_743);
 }
